17779827d9bbbacd5d61a1c20ef5e961c051c6b4
wangshuai <bjwangs@163.com>
2026-04-24 16:02:00 +0800

    refactor(car): 简化 CarServiceProvider

--- module/Car/CarServiceProvider.php
+++ module/Car/CarServiceProvider.php
@@ -7,44 +7,30 @@ use Route;
 
 class CarServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any package services.
-     */
     function boot()
     {
         $this->registerRoutes();
         $this->registerResources();
     }
 
-    /**
-     * Register the package routes.
-     */
     function registerRoutes()
     {
         Route::group($this->routeConfiguration(), function () {
             $this->loadRoutesFrom(__DIR__.'/api.php');
         });
-
     }
 
-    /**
-     * Get the Nova route group configuration array.
-     */
     protected function routeConfiguration(): array
     {
         return [
-            'namespace' => 'Module\Car\api',
+            'namespace' => 'Module\Car\API',
             'prefix' => 'api/car',
             'middleware' => 'auth:sanctum',
         ];
     }
 
-    /**
-     * Register the package resources.
-     */
     protected function registerResources()
     {
         $this->loadMigrationsFrom(__DIR__ . '/DB/Migrations');
     }
-
 }
